b512aec71e404071d0655b5b30e780629d9dd1fe
alfster2012 <foster.alfiecbbc@gmail.com>
2017-02-08 23:10:33 +0000

    add eval command

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "bonzi."
+const prefix = "=="
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -12,5 +12,10 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+if (message.content.startsWith(prefix + "eval")) {
+  message.reply(eval(message.content.split(" ").slice(1).join(" ")));
+};
+});
 
 client.login('Mjc4OTI3OTg0Mzc3ODU2MDAw.C3zbcA.DGbyBYKCOhPTl7rkEHFgtqRUz_8');
