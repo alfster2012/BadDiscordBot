70d04d69f7744db10886c2afbc8a92be2f11dacc
alfster2012 <foster.alfiecbbc@gmail.com>
2017-02-11 20:26:36 +0000

    added a few things

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-BadDiscordBot
+###BadDiscordBot
+
+Please create the file **settings.json** and add the following to it
+```json
+{
+    "token" : "[Your Bot Token]",
+    "prefix" : "[Your Bot Prefix" 
+}```
--- bot.js
+++ bot.js
@@ -1,29 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "=="
+const settings = require('./settings.json')
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === prefix + 'ping') {
+  if (message.content === settings.prefix + 'ping') {
     message.reply('pong');
   }
 });
 
 client.on('message', message => {
   if(message.author.id !== "135695463130071040") return;
-  if (message.content.startsWith(prefix + "eval")) {
+  if (message.content.startsWith(settings.prefix + "eval")) {
     message.reply(eval(message.content.split(" ").slice(1).join(" ")));
 };
 });
 
 client.on('message', message => {
-  if (message.content === prefix + 'avatar') {
+  if (message.content === settings.prefix + 'avatar') {
     message.reply(message.author.avatarURL);
   }
 });
 
 
-client.login('Mjc4OTI3OTg0Mzc3ODU2MDAw.C3zbcA.DGbyBYKCOhPTl7rkEHFgtqRUz_8');
+client.login('settings.token');
