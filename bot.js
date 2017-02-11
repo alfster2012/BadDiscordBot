ef85bbec97a365b443def9f26ac4036d7f37a69c
alfster2012 <foster.alfiecbbc@gmail.com>
2017-02-11 19:50:47 +0000

    Added avatar

--- bot.js
+++ bot.js
@@ -13,9 +13,17 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-if (message.content.startsWith(prefix + "eval")) {
-  message.reply(eval(message.content.split(" ").slice(1).join(" ")));
+  if(message.author.id !== "135695463130071040") return;
+  if (message.content.startsWith(prefix + "eval")) {
+    message.reply(eval(message.content.split(" ").slice(1).join(" ")));
 };
 });
+
+client.on('message', message => {
+  if (message.content === prefix + 'avatar') {
+    message.reply(message.author.avatarURL);
+  }
+});
+
 
 client.login('Mjc4OTI3OTg0Mzc3ODU2MDAw.C3zbcA.DGbyBYKCOhPTl7rkEHFgtqRUz_8');
